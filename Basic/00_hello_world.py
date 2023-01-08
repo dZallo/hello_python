84b6b9847e6fad0c88aaa9c662cd290ca620d076
dZallo <danielzallo@gmail.com>
2023-01-09 00:39:04 +0100

    print and data types

---
+++ Basic/00_hello_world.py
@@ -0,0 +1,20 @@
+# Comment in one line
+# Our hello world in python
+print("Hello World")
+"""
+You can make a multi-line comment
+Example
+"""
+'''
+You can make a multi-line comment
+Example
+'''
+# Data type
+print(type(10))                  # Int
+print(type(10.2))                # Float
+print(type(10+4j))               # Complex
+print(type("texto"))             # String - str
+print(type([1, 2, 3]))           # List
+print(type({'name':'Daniel'}))   # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
