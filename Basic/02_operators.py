ae17c3f5f5ee299e2ad18fd7164547e1e9bf87bc
dZallo <danielzallo@gmail.com>
2023-02-19 22:31:00 +0100

    operadores python

---
+++ Basic/02_operators.py
@@ -0,0 +1,42 @@
+### Operadores Aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2) # Modulo, para saber el resto
+print(10 // 2) # Division aproximando a un entero
+print(2**3) # Exponencial
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola" + "Python")
+print("Hola" + str(5))
+
+### Operadores Comparativos ###
+
+# Operaciones con enteros
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
+
+# Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
